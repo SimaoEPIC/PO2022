cb22e718f5108f77f6277ceaa62f6eac28c0ca6b
Simao Dias <simao.daniel.silva.dias@gmail.com>
2022-06-30 00:14:06 +0100

    Teste Sim

--- FazEverything/src/BACKEND/FazEverything.java
+++ FazEverything/src/BACKEND/FazEverything.java
@@ -15,7 +15,7 @@ public class FazEverything {
      */
     public static void main(String[] args) {
         System.out.println("O pila é gay!");
-        
+        System.out.println("A Perry é feia!");
         System.out.println("O capitale é burro!");
     }
     
